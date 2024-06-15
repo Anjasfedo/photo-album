6e49a7309780ded353edd6766b0d64987168843d
AnjasFedo <fedoaf98@gmail.com>
2024-06-15 17:52:47 +0700

    Do// Apply Tailwind CSS on Class

--- main.js
+++ main.js
@@ -10,16 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((data) => {
       data.forEach((photo) => {
         const photoDiv = document.createElement("div");
+        photoDiv.classList.add("div__image")
 
         const img = document.createElement("img");
+        img.classList.add("img__image")
         img.src = photo.thumbnailUrl;
         img.alt = photo.title;
 
         const title = document.createElement("h3");
+        title.classList.add("title__image")
         title.textContent = photo.title;
 
         const link = document.createElement("a");
-        link.classList.add("lorem");
+        link.classList.add("link__image");
         link.href = photo.url;
         link.textContent = "View Full Image";
 
